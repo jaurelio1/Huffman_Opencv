0909f774d1de12f143745891892db209c2ad3813
jaurelio1 <jaec35515@gmail.com>
2019-11-30 02:15:33 -0300

    Preparação para implementação da codificação de Huffman
    Criação da classe Node para os primeiros passos para criação da fila de prioridade, onde receberar o dado, a frequencia de vezes que esse dado aparece, um ponteiro(que necessita ver como se faz em python) para outros nós da esquerda e da direita.
    Implementação da funções de criação de heap máximo e do heap mínimo e de suas respectivas funções para construção.

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import cv2
 import numpy as np
 
+#construction of the node for the priority queue
+class Node:
+    def __init__(self, data, frequence, R, L):
+        self.data = data
+        self.frenquence = frequence
+        self.right = R
+        self.left = L
 
+
+#show the frequence of the pixels in the image
 def frequencePixels(image, height, weight):
     frenquece = np.zeros((256,), dtype=int)
 
@@ -10,8 +19,57 @@ def frequencePixels(image, height, weight):
             frenquece[image[i][j]] += 1
 
     return frenquece
+#..........................................
+
+#functions of construction of heaps
+#heap minimum for frequences
+def heapMin(S, i):
+    E = 2*i
+    D = (2*i) + 1
+    M = i
+
+    if(E <= (len(S)-1) and S[E] < S[M]):
+        M = E
+    if(D <= (len(S)-1) and S[D] < S[M]):
+        M = D
+
+    if(M != i):
+        aux = S[M]
+        S[M] = S[i]
+        S[i] = aux
+        heapMin(S, M)
+
+def builHeapMin(S):
+    for i in range(len(S)//2, 1, -1):
+        heapMin(S, i)
+#and heap maximum for Huffman coding
+def heapMax(S, i):
+    E = 2*i
+    D = (2*i) + 1
+    M = i
+
+    if(E <= (len(S)-1) and S[E] > S[M]):
+        M = E
+    if(D <= (len(S)-1) and S[D] > S[M]):
+        M = D
+
+    if(M != i):
+        aux = S[M]
+        S[M] = S[i]
+        S[i] = aux
+        heapMin(S, M)
+
+def buildHeapMax(S):
+    for i in range(len(S)//2, 1, -1):
+        heapMin(S, i)
+#................................................
+
+#functions of construction of priority queue
 
 
+#................................................
+
+#main
 #read the image
 image = cv2.imread("re-elmayer.jpg")
 
@@ -31,10 +89,15 @@ image_copy = cv2.cvtColor(image_copy, cv2.COLOR_RGB2GRAY)
 #transform a matrix into an array
 #image_copy = np.reshape(image_copy, (h_resized*w_resized))
 
-print(frequencePixels(image_copy, h_resized, w_resized))
+#array with frequence of pixels
+array_pixels = frequencePixels(image_copy, h_resized, w_resized)
+cpy_pixels = array_pixels.copy()
+
+builHeapMin(cpy_pixels)
+
+print(cpy_pixels)
 
 #display image
 #cv2.imshow('imaged', resized)
 cv2.waitKey(0)
 
-#alteração para ver se hevlla pode da committ

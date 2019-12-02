e748ab6c58fe26dc1b8b79deceed8808a4d18e6e
jaurelio1 <jaec35515@gmail.com>
2019-12-02 15:00:42 -0300

    Correções
    Acrescentando a classe Node, e mudanças na classe Heap...verificar o porque da funções da contrução do heap mínimo não esteja funcionando.

--- Heap.py
+++ Heap.py
@@ -5,13 +5,18 @@ class Heap:
 
     # heap minimum for frequences
     def heapMin(self, S, i):
-        E = 2 * i
-        D = (2 * i) + 1
-        M = i
+        if(i == 0):
+            E = 1
+            D = 2
+            M = 0
+        else:
+            E = 2 * i
+            D = (2 * i) + 1
+            M = i
 
-        if (E <= i and S[E] < S[M]):
+        if (E <= i and S[E].frequence < S[M].frequence):
             M = E
-        if (D <= i and S[D] < S[M]):
+        if (D <= i and S[D].frequence < S[M].frequence):
             M = D
 
         if (M != i):
@@ -20,10 +25,9 @@ class Heap:
             S[i] = aux
             self.heapMin(S, M)
 
-#Error 'list' object (S) has no attribute 'frequence'
     def buildHeapMin(self, S):
         for i in range(len(S) // 2, 0, -1):
-            self.heapMin(S.frequence, i)
+            self.heapMin(S, i)
     #..............................................
 
     def returnHeapMinimum(self):
---
+++ Node.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class Node:
+    frequences = np.zeros((256,), dtype=int)
+
+    def __init__(self, data=None, frequence=None, r=None, l=None, heap=[]):
+        self.H = heap
+        self.data = data
+        self.right = r
+        self.left = l
+        self.frequence = frequence
+
+    # show the frequence of the pixels in the image
+
+    def setFrequencePixels(self, image, height, weight):
+        for i in range(height):
+            for j in range(weight):
+                self.frequences[image[i][j]] += 1
+
+    # ..........................................
+
+    # store the nodes in an array
+    def storeNode(self):
+        for i in range(255):
+            #..................data, frequence of pixel
+            self.H.append(Node(i, self.frequences[i]))
+    # .............................................
+
+    def returnArrayNode(self):
+        self.storeNode()
+        return self.H
